65f9c8dee7594a9a7b684372bbc614d1bb642df6
AMALENDU MANOJ <90255760+amalendu123@users.noreply.github.com>
2024-07-07 09:51:39 +0530

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, patch, post, HttpResponse, HttpServer, Responder,App};
+
+#[get("/getblog")]
+async  fn get_blog() -> impl Responder{
+    HttpResponse::Ok().body("Blogs are available here")
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+    HttpServer::new(||{
+        App::new()
+            .service(get_blog)  
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
